bcb6ef6c4a0d517b457b48a1dca47a72237f7480
Silas Onyango <silas.onyango93@gmail.com>
2019-07-22 10:16:47 +0300

    initial commit

---
+++ src/main/java/com/silasonyango/personallibrary/models/UserModel.java
@@ -0,0 +1,73 @@
+package com.silasonyango.personallibrary.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.data.annotation.Transient;
+
+@Entity
+@Table(name = "users")
+public class UserModel {
+
+    @Column(name = "FirstName")
+    private String firstName;
+
+    @Column(name = "MiddleName")
+    private String middleName;
+
+    @Column(name = "Surname")
+    private String surname;
+
+    @Column(name = "Email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "EncryptedPassword")
+    @Transient
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
